e15aceb94daf4d4f248cb5fc2a68e9c59afbd14c
João Vitor Lansoni Barbosa <joaovitorvlb@hotmail.com>
2022-12-30 16:13:41 -0300

    Adicionado backend

---
+++ react-gram/backend/react-gram/config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASS;
+
+const conn = async () => {
+    try {
+        const dbConn = await mongoose.connect(
+            'mongodb://localhost:27017/myFirstDatabase'
+        );
+        console.log("Conectou ao banco de dados!");
+
+        return dbConn;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+conn();
+
+module.exports = conn;
